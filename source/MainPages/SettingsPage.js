02ad7764cc54adb1b2d43f9abf2c785ecedada4d
André Silva <75569750+AFA-silva@users.noreply.github.com>
2025-02-28 20:53:04 +0000

    SettingsPage Update

--- source/MainPages/SettingsPage.js
+++ source/MainPages/SettingsPage.js
@@ -1,11 +1,39 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import styles from '../Styles/MainPageStyles/SettingsPageStyle';
 
-const SettingsPage = () => {
+const SettingsPage = ({ navigation }) => {
+  const handleLogout = () => {
+    navigation.navigate('Login'); // Navegar para a página de login
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.contentText}>Settings Page</Text>
+      <Text style={styles.header}>Settings</Text>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Perfil</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Segurança</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Dark Mode</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Notificações</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Ajuda e Suporte</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
+        <Text style={styles.logoutText}>Logout</Text>
+      </TouchableOpacity>
     </View>
   );
 };
--- source/Styles/MainPageStyles/SettingsPageStyle.js
+++ source/Styles/MainPageStyles/SettingsPageStyle.js
@@ -3,13 +3,42 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#fff',
+    padding: 20,
   },
-  contentText: {
-    fontSize: 18,
-    color: '#333',
+  header: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    color: '#FF9800',
+    marginBottom: 30,
+    marginTop: 20,
+  },
+  button: {
+    width: '80%',
+    padding: 15,
+    backgroundColor: '#FFB300',
+    borderRadius: 10,
+    alignItems: 'center',
+    marginBottom: 15,
+  },
+  buttonText: {
+    fontSize: 16,
+    color: '#000',
+    fontWeight: 'bold',
+  },
+  logoutButton: {
+    width: '80%',
+    padding: 15,
+    backgroundColor: '#D84315',
+    borderRadius: 10,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  logoutText: {
+    fontSize: 16,
+    color: '#FFF',
+    fontWeight: 'bold',
   },
 });
 
